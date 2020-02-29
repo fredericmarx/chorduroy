5dbe938cd47cb921092a3cb87c3478f343d215d0
Frederic Marx <marx.frederic@gmail.com>
2020-02-29 16:08:35 +0100

    Initial commit

---
+++ .eleventy.js
@@ -0,0 +1,11 @@
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addPassthroughCopy("src/js");
+
+  return {
+    dir: {
+      input: "src",
+      output: "dist"
+    }
+  };
+};
---
+++ src/_includes/default.11ty.js
@@ -0,0 +1,16 @@
+module.exports = ({ content }) => `
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="/css/main.css">
+  </head>
+
+  <body>
+    ${content}
+    <script src="/js/main.js"></script>
+  </body>
+</html>`;
